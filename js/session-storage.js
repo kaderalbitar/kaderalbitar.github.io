32aa733ed9b90c3015853a02d9c7779560da951d
Abdul Kader <87765663+kaderalbitar@users.noreply.github.com>
2021-11-23 22:16:55 +0200

    Add files via upload

---
+++ js/session-storage.js
@@ -0,0 +1,15 @@
+if (window.sessionStorage) {
+  var txtUsername = document.getElementById('username');
+  var txtAnswer = document.getElementById('answer');
+
+  txtUsername.value = sessionStorage.getItem('username');
+  txtAnswer.value = sessionStorage.getItem('answer');
+
+  txtUsername.addEventListener('input', function() {
+    sessionStorage.setItem('username', txtUsername.value);
+  }, false);
+
+  txtAnswer.addEventListener('input', function() {
+    sessionStorage.setItem('answer', txtAnswer.value);
+  }, false);
+}
